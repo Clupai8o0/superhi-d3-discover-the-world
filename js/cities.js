25c301ec1fd18b49930087e3b39881e61e827316
Clupai8o0 <clupaio4@gmail.com>
2022-08-04 11:08:12 +0300

    making plotting dynamic

--- js/cities.js
+++ js/cities.js
@@ -4,11 +4,14 @@ const svg = d3.select("svg");
 svg.attr("width", 960).attr("height", 720);
 
 const placeCities = () => {
-	let valueX = "singlePerson";
-	let valueY = "apartment";
+	const inputX = document.querySelector("select[name=valueX]"),
+		inputY = document.querySelector("select[name=valueY]");
 
-	let maxValueX = d3.max(data, (d) => d[valueX]);
-	let maxValueY = d3.max(data, (d) => d[valueY]);
+	const valueX = inputX.value;
+	const valueY = inputY.value;
+
+	const maxValueX = d3.max(data, (d) => d[valueX]);
+	const maxValueY = d3.max(data, (d) => d[valueY]);
 
 	const scaleX = d3.scaleLinear().domain([0, maxValueX]).range([100, 860]);
 	const scaleY = d3.scaleLinear().domain([0, maxValueY]).range([620, 100]);
@@ -25,6 +28,12 @@ const placeCities = () => {
 			return `translate(${scaleX(x)}, ${scaleY(y)})`;
 		});
 	cities.append("circle").attr("cx", 0).attr("cy", 0).attr("r", 15);
+
+	svg.selectAll("g.city").transition().duration(500).attr("transform", (d) => {
+		const x = d[valueX],
+			y = d[valueY];
+		return `translate(${scaleX(x)}, ${scaleY(y)})`;
+	});
 };
 
 placeCities();
